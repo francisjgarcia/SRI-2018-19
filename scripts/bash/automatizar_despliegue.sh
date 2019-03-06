e38b87d9432bac91884c034141023eb251e43a70
Francis J. García <13500891+francisjgarcia@users.noreply.github.com>
2019-03-06 21:43:59 +0100

    Actualizado script de despliegue automático

--- scripts/bash/automatizar_despliegue.sh
+++ scripts/bash/automatizar_despliegue.sh
@@ -5,11 +5,20 @@ DockerHub="francisjgarcia"
 cd ..
 
 ## GENERANDO LAS IMÁGENES
-for Servicio in nginx apache mysql ftp postfix
+for Servicio in nginx apache mysql ftp
 do
 	echo "Generando imagen $DockerHub/$Servicio"
 	cd ../$Servicio
 	docker build -t $DockerHub/$Servicio .
+done
+
+## GENERANDO LAS IMÁGENES POSTFIX
+for Servicio in reenviador servidor
+do
+	echo "Generando imagen $DockerHub/$Servicio"
+	cd ../postfix/$Servicio
+	docker build -t $DockerHub/postfix$Servicio .
+	cd ..
 done
 
 ## Desplegando contenedores
